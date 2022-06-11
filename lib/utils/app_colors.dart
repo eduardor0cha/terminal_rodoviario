d23dd5c8f132be69bac5a7e441430f477c4c8e91
eduardor0cha <c.eduardo_rn@outlook.com>
2022-06-11 15:43:45 -0300

    Adicionar classe de gerenciamento das cores

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CoresApp {
+  static const _black = Color(0xFF151515);
+  static const _white = Color(0xFFF8F8F8);
+  static const _lightGray = Color(0xFFE6E6E6);
+  static const _gray = Color(0xFFA9A9A9);
+  static const _green = Color(0xFF04A701);
+  static const _red = Color(0xFFA51515);
+  static const _lightRed = Color(0xFFCF2B2B);
+  static const _whitishRed = Color(0xFFCF2B2B);
+
+  static get black => _black;
+  static get white => _white;
+  static get lightGray => _lightGray;
+  static get gray => _gray;
+  static get green => _green;
+  static get red => _red;
+  static get lightRed => _lightRed;
+  static get witishRed => _whitishRed;
+}
