4706596b61294e17faf6682ecbdb3babdad752a7
eduardor0cha <c.eduardo_rn@outlook.com>
2022-06-17 16:28:31 -0300

    Adicionar página de linhas e horários

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'pages/linhas_e_horarios.dart';
+
 void main() {
   runApp(const MaterialApp(
     home: null,
--- lib/pages/linhas_e_horarios.dart
+++ lib/pages/linhas_e_horarios.dart
@@ -1,15 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:terminal_rodoviario/widgets/barra_navegacao.dart';
+import 'package:terminal_rodoviario/widgets/viacao_card.dart';
 
+import '../domain/linha.dart';
+import '../domain/viacao.dart';
 import '../utils/app_colors.dart';
 
 class LinhasEHorarios extends StatefulWidget {
-  const LinhasEHorarios({ Key? key }) : super(key: key);
+  const LinhasEHorarios({Key? key}) : super(key: key);
 
   @override
   State<LinhasEHorarios> createState() => _LinhasEHorariosState();
 }
 
 class _LinhasEHorariosState extends State<LinhasEHorarios> {
+  final Viacao viacao1 = Viacao(
+    nome: "Real Arapiraca",
+    imagem:
+        "https://brt.onibusbrasil.com/2015/2/1/p/42e5aa01929b5be3b2023f96961559cc.jpg",
+    linhas: [
+      Linha(
+        pontoA: "IFAL",
+        pontoB: "Terminal",
+        horarios: [
+          "07:00",
+          "07:15",
+          "08:00",
+          "08:30",
+        ],
+        valor: 3.50,
+      ),
+      Linha(
+        pontoA: "IFAL",
+        pontoB: "Terminal",
+        horarios: [
+          "07:00",
+          "07:15",
+          "08:00",
+          "08:30",
+        ],
+        valor: 3.50,
+      ),
+      Linha(
+        pontoA: "IFAL",
+        pontoB: "Terminal",
+        horarios: [
+          "07:00",
+          "07:15",
+          "08:00",
+          "08:30",
+        ],
+        valor: 3.50,
+      ),
+    ],
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,13 +69,20 @@ class _LinhasEHorariosState extends State<LinhasEHorarios> {
             systemOverlayStyle: null,
             elevation: 0,
             title: Text(
-              'Viagens Marcadas',
+              'Linhas e horários',
               style: TextStyle(color: CoresApp.black),
             ),
           ),
         ),
       ),
-      body: ,
+      body: ListView(
+        children: [
+          ViacaoCard(
+            viacao: viacao1,
+          ),
+        ],
+      ),
+      bottomNavigationBar: const BarraNavegacao(index: 1),
     );
   }
 }
