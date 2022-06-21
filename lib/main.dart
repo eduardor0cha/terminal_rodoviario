4ea3881b1258017924fc24bcfd1429479626dc9d
eduardor0cha <c.eduardo_rn@outlook.com>
2022-06-20 21:14:13 -0300

    Adicionar fonte

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:terminal_rodoviario/pages/promocoes.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Promocoes(),
+  runApp(MaterialApp(
+    theme: ThemeData(fontFamily: 'Open Sans'),
+    home: const Promocoes(),
   ));
 }
